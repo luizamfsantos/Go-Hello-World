f8ba9dc42bef6d7fc400f67d396d618014eee216
luizamfsantos <luiza.marques_@hotmail.com>
2024-09-14 15:07:45 -0500

    different values in go

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang") // golang
+	fmt.Println("1+1 = ", 1 + 1) // 1 + 1 = 2
+	fmt.Println(true && false) // false
+	fmt.Println(true || false) // true
+	fmt.Println(!true) // false
+}
