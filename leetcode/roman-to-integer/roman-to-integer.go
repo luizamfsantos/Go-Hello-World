1b0ff2c41429e148699a442d2fecfc317f3df9f3
luizamfsantos <luiza.marques_@hotmail.com>
2024-09-21 10:42:37 -0300

    complete approach handle smaller vals before bigger

--- leetcode/roman-to-integer/roman-to-integer.go
+++ leetcode/roman-to-integer/roman-to-integer.go
@@ -13,11 +13,24 @@ var roman_mapping = map[string]int{
 }
 
 func romanToInt(s string) int {
-	var total int
+	var total, new_val, previous_val int
 	for i := 0; i < len(s); i++ {
 		string_subset := string(s[i])
-		val := roman_mapping[string_subset]
-		total += val
+		switch i {
+		case 0:
+			new_val := roman_mapping[string_subset]
+			total += new_val
+			previous_val = new_val
+		default:
+			previous_val = new_val
+			new_val = roman_mapping[string_subset]
+			if previous_val < new_val {
+				total += new_val
+				total -= 2 * previous_val
+			} else {
+				total += new_val
+			}
+		}
 	}
 
 	return total
@@ -35,4 +48,9 @@ func main() {
 	string3 := "MCMXCIV"
 	answer3 := romanToInt(string3)
 	fmt.Printf("%s is %d\n", string3, answer3) // MCMXCIV is 1994
+
+	string4 := "IV"
+	answer4 := romanToInt(string4)
+	fmt.Printf("%s is %d\n", string4, answer4) // IV is 4
+
 }
